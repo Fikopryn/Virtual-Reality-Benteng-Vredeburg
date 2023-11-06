f008a6724da2302785dab23c9f1e9d4a957c1678
Fiko <fikopriyono27@gmail.com>
2023-11-06 20:32:54 +0700

    Asset dan Bahan

---
+++ Assets/Scripts/SceneCtrl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCtrl : MonoBehaviour{ 
+     
+    public void ChangeScene(string SampleScene)
+    {
+
+        SceneManager.LoadScene(SampleScene);
+
+ 
+    }
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
